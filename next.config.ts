338ac75786b26d42fa07785ef1e1e376a9138c1e
thibault <tjeanpierre757@gmail.com>
2026-04-08 19:34:52 +0200

    [FEAT] display version
    https://tjeanpierre757.atlassian.net/browse/PUL-58

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { version } = require("./package.json");
+
 const nextConfig: NextConfig = {
+    env: {
+        NEXT_PUBLIC_APP_VERSION: version,
+    },
     async headers() {
         return [
             {
